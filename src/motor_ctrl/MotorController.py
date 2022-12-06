2417f35dff6d77a1b6246b15aecd2910b323e7d0
JosePCarvalho <78820554+Jose-PCarvalho@users.noreply.github.com>
2022-12-06 21:57:11 +0000

    bugfix

--- src/motor_ctrl/MotorController.py
+++ src/motor_ctrl/MotorController.py
@@ -10,6 +10,7 @@ def init_pwm():
     wiringpi.wiringPiSetupGpio()
     wiringpi.pwmSetClock(192)
     wiringpi.pwmSetRange(2000)
+    wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
 
 class Motor:
     def __init__(self,pin):
@@ -17,7 +18,8 @@ class Motor:
         self.base_pwm = 150 #1500 us
         self.pin = pin
         init_pwm()
-        wiringpi.pwmWrite(pin, self.base_pwm)
+        wiringpi.pinMode(self.pin, wiringpi.GPIO.PWM_OUTPUT)
+        wiringpi.pwmWrite(self.pin, self.base_pwm)
         time.sleep(7) ##iniciar ESC
         print("Initiating Motor")
 
