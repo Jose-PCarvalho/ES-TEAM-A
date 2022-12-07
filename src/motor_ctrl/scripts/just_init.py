aaeb9746276e1d3455857ad05dc57ea97a5bacc6
Laroto <up201806660@fe.up.pt>
2022-12-07 18:50:23 +0000

    just init

---
+++ src/motor_ctrl/scripts/just_init.py
@@ -0,0 +1,24 @@
+import sys
+import wiringpi
+import time
+
+print("\nInit Service Called")
+    
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+
+# set #18 to be a PWM output
+wiringpi.pinMode(13, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+# divide down clock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+
+delay_period = 0.01
+wiringpi.pwmWrite(13, 150)
+wiringpi.pwmWrite(18, 150)
+time.sleep(7)
