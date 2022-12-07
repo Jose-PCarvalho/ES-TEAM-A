9180933eece02c6a7d37fba6f4d24bc1bf524b39
Laroto <up201806660@fe.up.pt>
2022-12-07 18:57:59 +0000

    small fix

--- src/motor_ctrl/scripts/init_server.py
+++ src/motor_ctrl/scripts/init_server.py
@@ -39,4 +39,23 @@ def init_motors_server():
     rospy.spin()
 
 if __name__ == "__main__":
+    print("Initializing")
+    # use 'GPIO naming'
+    wiringpi.wiringPiSetupGpio()
+
+    # set #18 to be a PWM output
+    wiringpi.pinMode(13, wiringpi.GPIO.PWM_OUTPUT)
+    wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+
+    # set the PWM mode to milliseconds stype
+    wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+    # divide down clock
+    wiringpi.pwmSetClock(192)
+    wiringpi.pwmSetRange(2000)
+
+    delay_period = 0.01
+    wiringpi.pwmWrite(13, 150)
+    wiringpi.pwmWrite(18, 150)
+    time.sleep(7)
     init_motors_server()
