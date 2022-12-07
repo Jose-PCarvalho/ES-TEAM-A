b767065e6621a9d062b3112b84d374fcc9a9135d
Laroto <up201806660@fe.up.pt>
2022-12-07 19:15:44 +0000

    test

--- src/motor_ctrl/scripts/init_server.py
+++ src/motor_ctrl/scripts/init_server.py
@@ -2,6 +2,7 @@ import rospy
 import sys
 import wiringpi
 import time
+import os
 
 from motor_ctrl.srv import init_motors, init_motorsResponse
 
@@ -10,24 +11,7 @@ def init_fun(req):
     #print("teste: [%s]"%(req.req,))
     print("\nInit Service Called")
     
-    # use 'GPIO naming'
-    wiringpi.wiringPiSetupGpio()
-
-    # set #18 to be a PWM output
-    wiringpi.pinMode(13, wiringpi.GPIO.PWM_OUTPUT)
-    wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
-
-    # set the PWM mode to milliseconds stype
-    wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
-
-    # divide down clock
-    wiringpi.pwmSetClock(192)
-    wiringpi.pwmSetRange(2000)
-
-    delay_period = 0.01
-    wiringpi.pwmWrite(13, 150)
-    wiringpi.pwmWrite(18, 150)
-    time.sleep(7)
+    os.system("echo ubuntu | sudo -S python3 just_init.py")
     
     return True
 
