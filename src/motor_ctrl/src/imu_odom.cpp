881d45fc54e11e037b02846dce6a0b80411fdf83
Laroto <up201806660@fe.up.pt>
2022-12-13 22:44:23 +0000

    estimate velocity

--- src/motor_ctrl/src/imu_odom.cpp
+++ src/motor_ctrl/src/imu_odom.cpp
@@ -1,20 +1,36 @@
 #include <ros/ros.h>
 #include <iostream>
+#include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Imu.h>
 
 sensor_msgs::Imu new_imu_data, old_imu_data;
 bool first_run = true;
+float freq;
+ros::Publisher pub;
 
 void callback (const sensor_msgs::ImuConstPtr &msg)
 {
-    if (first_run)
+    if (first_run==true)
     {
         new_imu_data = *msg;
         first_run = false;
         return;
     }
-    
 
+    old_imu_data = new_imu_data;
+    new_imu_data = *msg;
+
+    geometry_msgs::Twist twist_msg;
+
+    twist_msg.angular.x = new_imu_data.angular_velocity.x;
+    twist_msg.angular.y = new_imu_data.angular_velocity.y;
+    twist_msg.angular.z = new_imu_data.angular_velocity.z;
+
+    twist_msg.linear.x = (new_imu_data.linear_acceleration.x - old_imu_data.linear_acceleration.x) / freq;
+    twist_msg.linear.y = (new_imu_data.linear_acceleration.y - old_imu_data.linear_acceleration.y) / freq;
+    twist_msg.linear.z = (new_imu_data.linear_acceleration.z - old_imu_data.linear_acceleration.z) / freq;
+
+    pub.publish(twist_msg);
 }
 
 int main(int argc, char** argv)
@@ -23,6 +39,9 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
     ros::Subscriber sub;
     sub = nh.subscribe("/imu_data", 1, callback);
+    pub = nh.advertise<geometry_msgs::Twist>("/vel_estimate", 1);
+
+    nh.param<float>("/freq", freq, 10);
 
     ros::Rate rate(10);
 
