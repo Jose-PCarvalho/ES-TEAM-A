1271e74bffe9c23537f86502c14751e50628e103
Jose-PCarvalho <up201806263@fe.up.pt>
2022-12-14 00:26:19 +0000

    ..

--- src/motor_ctrl/scripts/pid_controller.py
+++ src/motor_ctrl/scripts/pid_controller.py
@@ -95,8 +95,8 @@ def timer_callback(event):
     #controller.motor_speed(v=-1,w=0)
     #print("Motors: ")
     #print(controller.u1,controller.u2)
-    controller.u1=controller.v_ref*10+controller.w_ref*100
-    controller.u2=controller.w_ref*10-controller.w_ref*100
+    controller.u1=controller.v_ref*100+controller.w_ref*1000
+    controller.u2=controller.w_ref*100-controller.w_ref*1000
     print(controller.u1,controller.u2)
     set_fun(controller.u1,controller.u2)
 
