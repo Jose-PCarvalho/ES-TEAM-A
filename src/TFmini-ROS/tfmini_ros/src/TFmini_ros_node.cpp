8ace1e7b2099a1c82169c096f5bbab101574d8df
Laroto <up201806660@fe.up.pt>
2023-01-16 23:48:41 +0000

    lidar

--- src/TFmini-ROS/tfmini_ros/src/TFmini_ros_node.cpp
+++ src/TFmini-ROS/tfmini_ros/src/TFmini_ros_node.cpp
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
   std::system("echo ubuntu | sudo -S gpio write 5 1");
 
   nh.param("serial_port", portName, std::string("/dev/ttyUSB0"));
-  nh.param("baud_rate", baud_rate, 9600);
+  nh.param("baud_rate", baud_rate, 115200);
 
   tfmini_obj = new benewake::TFmini(portName, baud_rate);
   ros::Publisher pub_range = nh.advertise<sensor_msgs::Range>(id, 1, true);
