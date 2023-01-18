50351390f879f18c6cca4eabddaaf361a2b16c31
Jose-PCarvalho <up201806263@fe.up.pt>
2023-01-18 14:48:04 +0000

    ...

--- src/ros-mpu9250-ahrs/mpu9250-driver/scripts/mpu9250.py
+++ src/ros-mpu9250-ahrs/mpu9250-driver/scripts/mpu9250.py
@@ -139,9 +139,9 @@ def talker():
             acc_msg.y=ay
             acc_msg.z=az
 
-            imu_msg.linear_acceleration_covariance[0] = 0.1
-            imu_msg.linear_acceleration_covariance[4] = 0.1
-            imu_msg.linear_acceleration_covariance[8] = 0.1
+            imu_msg.linear_acceleration_covariance[0] = 1
+            imu_msg.linear_acceleration_covariance[4] = 1
+            imu_msg.linear_acceleration_covariance[8] = 1
             imu_pub.publish(imu_msg)
             mag_pub.publish(mag_msg)
             magn_pub.publish(mag_calib_msg)
