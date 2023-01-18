9c009643fac66f5ba167072ef1258a940e4f5991
Laroto <up201806660@fe.up.pt>
2023-01-18 01:46:47 +0000

    changes

--- src/control/scripts/sound.py
+++ src/control/scripts/sound.py
@@ -3,21 +3,44 @@ import rospy
 import numpy as np
 import os
 import time
-from geometry_msgs.msg import Twist
+from std_msgs.msg import Int8
+import rospkg
+
+# flag =True
+
+# def timer_callback(event): 
+#     global flag
+#     #print("timer")
+#     flag = True
 
 def callback(data):
-    if data.data == True:
-        print("true")
+    global flag
+    if data.data == 3:
+        rospack = rospkg.RosPack()
+        path = rospack.get_path('control')
+        path = path + '/scripts/sound.mp3'
+        playsound(path)
+        time.sleep(10)
+        # if (flag == True):
+        #     #print("ok")
+        #     rospack = rospkg.RosPack()
+        #     path = rospack.get_path('control')
+        #     path = path + '/scripts/sound.mp3'
+        #     playsound(path)
+        #     timer = rospy.Timer(rospy.Duration(10), timer_callback)
+        #     flag = False
+
 
 
 
 def listener():
     rospy.init_node('sound', anonymous=True)
-    rospy.Subscriber ('/mision/state',int,callback)    
+    rospy.Subscriber ('/mission/state',Int8,callback, queue_size=1)    
     #timer = rospy.Timer(rospy.Duration(0.1), timer_callback)
     #print ("Last message published")
     rospy.spin()    
 
 
 if __name__ == '__main__':
+    
     listener()
